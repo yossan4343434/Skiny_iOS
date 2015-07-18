1818cc58782f4e8b7d3fb8e9673b6fa9a65e17b8
yossan4343434 <yossan4343434@gmail.com>
2015-07-18 23:20:27 +0900

    Display dummyContents

--- Skiny_iOS/SKCosmeticListViewController.swift
+++ Skiny_iOS/SKCosmeticListViewController.swift
@@ -8,28 +8,29 @@
 
 import UIKit
 
-class SKCosmeticListViewController: UIViewController {
+class SKCosmeticListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var dummyContents = NSArray()
+
+    @IBOutlet weak var cosmeticListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dummyContents = ["hoge", "foo", "bar"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyContents.count
     }
-    */
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+
+        return cell
+    }
 
 }
