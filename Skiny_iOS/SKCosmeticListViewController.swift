8e9c6976226496bcb9e5c761ce0b1f46a9ceb210
yossan4343434 <yossan4343434@gmail.com>
2015-07-18 23:10:45 +0900

    Create SKCosmeticViewController

---
+++ Skiny_iOS/SKCosmeticListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SKCosmeticListViewController.swift
+//  Skiny_iOS
+//
+//  Created by Yoshiyuki Kuga on 2015/07/18.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKCosmeticListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
