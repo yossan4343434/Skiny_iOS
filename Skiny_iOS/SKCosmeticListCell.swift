4316d38b1f9cc0ee78272780fa2715fac58f7320
yossan4343434 <yossan4343434@gmail.com>
2015-07-18 23:33:50 +0900

    Create SKCosmeticListCell

---
+++ Skiny_iOS/SKCosmeticListCell.swift
@@ -0,0 +1,24 @@
+//
+//  SKCosmeticListCell.swift
+//  Skiny_iOS
+//
+//  Created by Yoshiyuki Kuga on 2015/07/18.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKCosmeticListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
