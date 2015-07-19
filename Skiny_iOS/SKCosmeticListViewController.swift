0eeac6c08e9e761bd38348b5b87c1b7bd32779ad
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-19 20:09:38 +0900

    Display dummyCosmetics on cosmeticListViewController

--- Skiny_iOS/SKCosmeticListViewController.swift
+++ Skiny_iOS/SKCosmeticListViewController.swift
@@ -10,31 +10,61 @@ import UIKit
 
 class SKCosmeticListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var dummyContents = NSArray()
+    var dummyCosmetics = [SKCosmetic]()
 
     @IBOutlet weak var cosmeticListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dummyContents = ["hoge", "foo", "bar"]
 
         let nib: UINib = UINib(nibName: "SKCosmeticListCell", bundle: nil)
         cosmeticListTableView.registerNib(nib, forCellReuseIdentifier: "cosmeticListCell")
 
         cosmeticListTableView.delegate = self
         cosmeticListTableView.dataSource = self
+
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func loadData() {
+        if let path = NSBundle.mainBundle().pathForResource("dummy_contents", ofType: "json") {
+            if let jsonData = NSData(contentsOfFile: path) {
+                let jsonResult = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments, error: nil) as! NSDictionary
+                let jsonResultCosmetics = jsonResult["cosmetic"] as! NSArray
+
+                for jsonResultCosmetic in jsonResultCosmetics {
+                    var dummyCosmetic = SKCosmetic()
+
+                    dummyCosmetic.id = jsonResultCosmetic["id"] as! Int
+                    dummyCosmetic.name = jsonResultCosmetic["name"] as! String
+                    dummyCosmetic.brand = jsonResultCosmetic["brand"] as! String
+                    dummyCosmetic.category = jsonResultCosmetic["category"] as! String
+                    dummyCosmetic.image = jsonResultCosmetic["image"] as! String
+
+                    dummyCosmetics.append(dummyCosmetic)
+                }
+            }
+        }
+    }
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dummyContents.count
+        return dummyCosmetics.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: SKCosmeticListCell = tableView.dequeueReusableCellWithIdentifier("cosmeticListCell", forIndexPath: indexPath) as! SKCosmeticListCell
+
+        var dummyCosmetic = dummyCosmetics[indexPath.row]
+
+        cell.cosmeticNameLabel.text = dummyCosmetic.name as String
+        cell.cosmeticBrandLabel.text = dummyCosmetic.brand as String
+        cell.cosmeticCategoryLabel.text = dummyCosmetic.category as String
+        let imageName = NSString(string: dummyCosmetic.image as String)
+        cell.cosmeticImageView.image = UIImage(named: imageName as String)
 
         return cell
     }
