fbaa033cd6c0e2915a08fa37d3b16674fe43a95e
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-20 08:43:26 +0900

    Create SKSelectCosmeticViewController and connect it with SKCosmeticListViewController

---
+++ Skiny_iOS/SKSelectCosmeticViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SKSelectCosmeticViewController.swift
+//  Skiny_iOS
+//
+//  Created by Yoshiyuki Kuga on 2015/07/20.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKSelectCosmeticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
