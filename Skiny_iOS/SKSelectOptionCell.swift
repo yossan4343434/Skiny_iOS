3956e0517915e87d0ca65a45c220eb5878cb58d6
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-20 09:03:44 +0900

    Add label to selectOptionCell

--- Skiny_iOS/SKSelectOptionCell.swift
+++ Skiny_iOS/SKSelectOptionCell.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class SKSelectOptionCell: UITableViewCell {
 
+    @IBOutlet weak var optionLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
 
-        // Configure the view for the selected state
+    class func cellHeight() -> CGFloat {
+        return 46
     }
     
 }
