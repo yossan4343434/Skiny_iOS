40ff16bd945182467a4b143ba38fed582db62a32
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-20 12:06:48 +0900

    Add select brand & category method according to selectType

--- Skiny_iOS/SKSelectCosmeticViewController.swift
+++ Skiny_iOS/SKSelectCosmeticViewController.swift
@@ -10,18 +10,32 @@ import UIKit
 
 class SKSelectCosmeticViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var selectOptionTableView: UITableView!
+    @IBOutlet weak var selectCategoryTableView: UITableView!
+    @IBOutlet weak var selectBrandTableView: UITableView!
+
+    var selectType = Int() // 0: select category, 1: select brand
 
     var categories = [SKCategory]()
+    var selectedCategory = SKCategory()
+    var brands = [SKBrand]()
+    var selectedBrands = SKBrand()
+    var cosmetics = [SKCosmetic]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let nib: UINib = UINib(nibName: "SKSelectOptionCell", bundle: nil)
-        selectOptionTableView.registerNib(nib, forCellReuseIdentifier: "selectOptionCell")
+        let nib = UINib(nibName: "SKSelectOptionCell", bundle: nil)
+        if selectType == 0 {
+            selectCategoryTableView.registerNib(nib, forCellReuseIdentifier: "selectOptionCell")
 
-        selectOptionTableView.delegate = self
-        selectOptionTableView.dataSource = self
+            selectCategoryTableView.delegate = self
+            selectCategoryTableView.dataSource = self
+        } else if selectType == 1 {
+            selectBrandTableView.registerNib(nib, forCellReuseIdentifier: "selectOptionCell")
+
+            selectBrandTableView.delegate = self
+            selectBrandTableView.dataSource = self
+        }
 
         loadData()
     }
@@ -34,30 +48,77 @@ class SKSelectCosmeticViewController: UIViewController, UITableViewDelegate, UIT
         if let path = NSBundle.mainBundle().pathForResource("dummy_contents", ofType: "json") {
             if let jsonData = NSData(contentsOfFile: path) {
                 let jsonResult = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments, error: nil) as! NSDictionary
-                let jsonResultCategories = jsonResult["categories"] as! NSArray
+                if selectType == 0 {
+                    let jsonResultCategories = jsonResult["categories"] as! NSArray
 
-                for jsonResultCategory in jsonResultCategories {
-                    var category = SKCategory()
+                    for jsonResultCategory in jsonResultCategories {
+                        var category = SKCategory()
 
-                    category.id = jsonResultCategory["id"] as! Int
-                    category.name = jsonResultCategory["name"] as! String
+                        category.id = jsonResultCategory["id"] as! Int
+                        category.name = jsonResultCategory["name"] as! String
 
-                    categories.append(category)
+                        categories.append(category)
+                    }
+                } else if selectType == 1 {
+                    var jsonResultCosmetics = jsonResult["cosmetics"] as! NSArray
+                    var jsonResultBrands = jsonResult["brands"] as! NSArray
+
+                    for jsonResultCosmetic in jsonResultCosmetics {
+                        let jsonResultCosmeticCategoryId = jsonResultCosmetic["category"] as! Int
+                        if jsonResultCosmeticCategoryId == selectedCategory.id {
+                            var brandId = jsonResultCosmetic["brand"] as! Int
+
+                            var brand = SKBrand()
+                            brand.id = jsonResultBrands[brandId - 1]["id"] as! Int
+                            brand.name = jsonResultBrands[brandId - 1]["name"] as! String
+
+                            brands.append(brand)
+                        }
+                    }
                 }
             }
         }
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        var rowsCount = Int()
+
+        if selectType == 0 {
+            rowsCount = categories.count
+        } else if selectType == 1 {
+            rowsCount = brands.count
+        }
+
+        return rowsCount
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: SKSelectOptionCell = tableView.dequeueReusableCellWithIdentifier("selectOptionCell", forIndexPath: indexPath) as! SKSelectOptionCell
 
-        cell.optionLabel.text = categories[indexPath.row].name as String
+        if selectType == 0 {
+            cell.optionLabel.text = categories[indexPath.row].name as String
+        } else if selectType == 1 {
+            cell.optionLabel.text = brands[indexPath.row].name as String
+        }
 
         return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if selectType == 0 {
+            selectedCategory = categories[indexPath.row]
+            performSegueWithIdentifier("toSelectBrand", sender: nil)
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "toSelectBrand" {
+            var selectCosmeticViewController = SKSelectCosmeticViewController()
+            selectCosmeticViewController = segue.destinationViewController as! SKSelectCosmeticViewController
+            selectCosmeticViewController.selectType = 1
+            selectCosmeticViewController.selectedCategory = selectedCategory
+        }
     }
 
 }
