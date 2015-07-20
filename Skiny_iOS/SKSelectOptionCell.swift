cc16e4a58465e86cf63128e8a39c6d5a6726a70e
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-20 09:01:48 +0900

    Create SKSelectOptionCell

---
+++ Skiny_iOS/SKSelectOptionCell.swift
@@ -0,0 +1,24 @@
+//
+//  SKSelectOptionCell.swift
+//  Skiny_iOS
+//
+//  Created by Yoshiyuki Kuga on 2015/07/20.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKSelectOptionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
