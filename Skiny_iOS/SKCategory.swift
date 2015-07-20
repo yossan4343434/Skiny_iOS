74e97c3a2ec37cdd1b0275649e5c38d022775f89
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-07-20 10:49:56 +0900

    Create SKCategory & SKBrand

---
+++ Skiny_iOS/SKCategory.swift
@@ -0,0 +1,14 @@
+//
+//  SKCategory.swift
+//  Skiny_iOS
+//
+//  Created by Yoshiyuki Kuga on 2015/07/20.
+//  Copyright (c) 2015年 Gruppy. All rights reserved.
+//
+
+import UIKit
+
+class SKCategory: NSObject {
+    var id = Int()
+    var name = String()
+}
